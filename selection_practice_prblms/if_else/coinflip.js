58fd0649b5e058459a57f4c5bad220f3dc374983
sakshi02v <sakshi02v@gmail.com>
2025-03-04 16:24:54 +0530

    D

--- selection_practice_prblms/if_else/coinflip.js
+++
@@ -1,9 +0,0 @@
-// Generate a random number between 1 and 2
-let currState = Math.floor(Math.random()*2)+1;
-
-if(currState == 1) {
-    console.log("Tails");
-}
-else if(currState == 2) {
-    console.log("Heads");
-}
