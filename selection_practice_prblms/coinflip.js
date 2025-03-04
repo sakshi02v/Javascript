bbdb6a82a548d29567e26533dfce57736b422025
Sakshi Verma <sakshi02v@gmail.com>
2025-03-04 13:52:58 +0530

    [Sakshi Verma]Add. sum and average calculate

---
+++ selection_practice_prblms/coinflip.js
@@ -0,0 +1,9 @@
+// Generate a random number between 1 and 2
+let currState = Math.floor(Math.random()*2)+1;
+
+if(currState == 1) {
+    console.log("Tails");
+}
+else if(currState == 2) {
+    console.log("Heads");
+}
--- sequence_practice_prblms/sumaverage.js
+++ sequence_practice_prblms/sumaverage.js
@@ -9,7 +9,7 @@ for (let i = 0; i < 5; i++) {
 let sum = numbers.reduce((acc, num) => acc + num, 0);
 let average = sum / numbers.length;
 
-// Print results
+// results
 console.log("Generated Numbers:", numbers);
 console.log("Sum:", sum);
 console.log("Average:", average.toFixed(2));
