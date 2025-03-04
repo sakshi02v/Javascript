aa9c033f90aa554621316c9d6dd3e299ac95367d
Sakshi Verma <sakshi02v@gmail.com>
2025-03-04 13:32:55 +0530

    [Sakshi Verma]Add. sum and average calculation

---
+++ sequence_practice_prblms/sumaverage.js
@@ -0,0 +1,15 @@
+// Generate 5 random two-digit numbers
+let numbers = [];
+for (let i = 0; i < 5; i++) {
+    let randomNumber = Math.floor(Math.random() * 90) + 10; // 10 to 99
+    numbers.push(randomNumber);
+}
+
+// Calculate sum and average
+let sum = numbers.reduce((acc, num) => acc + num, 0);
+let average = sum / numbers.length;
+
+// Print results
+console.log("Generated Numbers:", numbers);
+console.log("Sum:", sum);
+console.log("Average:", average.toFixed(2));
